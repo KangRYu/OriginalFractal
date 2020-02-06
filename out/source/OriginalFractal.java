32cfbbf6be4a67d9b129822529fd50a7288fe534
Kang Yu <karuiyu@gmail.com>
2020-02-06 11:03:26 -0800

    Finished fractal flower

--- out/source/OriginalFractal.java
+++ out/source/OriginalFractal.java
@@ -1,8 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 public void setup() {
-    size(500, 500);
+    
     noStroke();
     rectMode(CENTER);
     fractal(250, 250, 10, 0);
+}
+
+public void draw() {
+    fractal(width/2, height/2, 10, 0);
 }
 
 public void fractal(float x, float y, float length, float angle) {
@@ -24,4 +44,14 @@ public void fractal(float x, float y, float length, float angle) {
 
         fractal(x, y, length + 1, angle - 1);
     }
+}
+  public void settings() {  size(500, 500); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
